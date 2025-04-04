c05f718809e5575f44728656dc7f5d64607938f8
IxchelChvz <104280360+IxchelChvz@users.noreply.github.com>
2025-04-04 18:03:48 +0000

    Elimine el archivo page3 y del app

--- page3.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("📈Progress Tracker")
-st.subheader("Weight Lifted Over Time")
-st.subheader("BMI Trend Over Time")
--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ pages = {
         st.Page("home.py", title="Home"),
         st.Page("page1.py", title="BMI Tracker"),
         st.Page("page2.py", title="Workout Logger"),
-        st.Page("page3.py", title="Progess Tracker"),
     ]
 }
 
